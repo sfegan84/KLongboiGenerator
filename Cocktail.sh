d71e4708f1eb31a75b5cf580426ff9676e021301
Dr M Bashkanov <mikhail.bashkanov@york.ac.uk>
2025-06-17 14:37:31 +0100

    hddm add added

--- Cocktail.sh
+++ Cocktail.sh
@@ -24,6 +24,16 @@ KLGenerator_hddm_V3 -M${htmp} -Fgenerated.root -Ekaon:histo:0.0:10.0 -Rkl2 -Ssol
 mv generated.hddm ./tmp/kl2.hddm
 mv generated.root ./tmp/kl2.root
 
+#kl3
+htmp=$((Hr*235));
+
+echo $Hr " " $htmp 
+
+KLGenerator_hddm_V3 -M${htmp} -Fgenerated.root -Ekaon:histo:0.0:10.0 -Rkl3 -SsolC
+
+mv generated.hddm ./tmp/kl3.hddm
+mv generated.root ./tmp/kl3.root
+
 #kl4
 htmp=$((Hr*14479));
 
@@ -65,8 +75,9 @@ mv generated.hddm ./tmp/kl9.hddm
 mv generated.root ./tmp/kl9.root
 
 rm ./Cocktail_kl.root
-hadd ./Cocktail_kl.root ./tmp/kl1.root ./tmp/kl2.root ./tmp/kl4.root ./tmp/kl5.root ./tmp/kl6.root  ./tmp/kl9.root 
+hadd ./Cocktail_kl.root ./tmp/kl1.root ./tmp/kl2.root ./tmp/kl3.root ./tmp/kl4.root ./tmp/kl5.root ./tmp/kl6.root  ./tmp/kl9.root 
 
+hddm_merge_files -oCocktail_kl.hddm ./tmp/kl1.hddm  ./tmp/kl2.hddm  ./tmp/kl3.hddm  ./tmp/kl4.hddm  ./tmp/kl5.hddm  ./tmp/kl6.hddm  ./tmp/kl9.hddm
 #done
 
 #num1=10
